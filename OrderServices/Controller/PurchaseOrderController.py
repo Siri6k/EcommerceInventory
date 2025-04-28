e97ae479ff34beae3b254a257f6d665ed647433c
sirisk <adamchrisk@gmail.com>
2025-04-28 13:51:12 +0200

    create purchase order Endpoint

--- OrderServices/Controller/PurchaseOrderController.py
+++ OrderServices/Controller/PurchaseOrderController.py
@@ -1,13 +1,16 @@
+import re
 from rest_framework import generics, serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from OrderServices.models import PurchaseOrder, PurchaseOrderItems, PurchaseOrderLogs
-from EcommerceInventory.Helpers import getDynamicFormFields, renderResponse
+from EcommerceInventory.Helpers import CommonListAPIMixin, CustomPageNumberPagination, getDynamicFormFields, renderResponse
 
 
 
 class PurchaseOrderItemSerializer(serializers.ModelSerializer):
+    sku = serializers.CharField(source="product_id.sku", read_only=True)
+    product_name = serializers.CharField(source="product_id.name", read_only=True)
     class Meta:
         model=PurchaseOrderItems
         fields="__all__"
@@ -40,14 +43,83 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
         )
         purchaseOrderLog.save()
         return purchaseOrder
+    def update(self, instance, validated_data):
+        items_data = validated_data.pop('po_id_purchase_order_items')
+        instance=super().update(
+            instance=instance,
+            validated_data=validated_data
+            )
+        items=[item_data.get('id') for item_data in items_data if 'id' in item_data]
+        PurchaseOrderItems.objects.filter(
+            po_id=instance
+        ).exclude(id__in=items).delete()
+
+        for item_data in items_data:
+            item_data.update({"created_by_user_id":validated_data.get("created_by_user_id")})
+            item_data.update({"domain_user_id":validated_data.get("domain_user_id")})
+
+            if 'po_id' in item_data:
+                item_data.pop('po_id')
+
+            if 'id' in item_data:
+                poItem=PurchaseOrderItems.objects.filter(
+                    id = item_data.get('id')
+                )
+                po_item_item_serializer = PurchaseOrderItemSerializer(
+                    poItem.first(),
+                    data=item_data
+                    )
+                if po_item_item_serializer.is_valid():
+                    po_item_item_serializer.save()
+            else:
+                PurchaseOrderItems.objects.create(
+                po_id=instance,
+                **item_data
+            )
+                
+        purchaseOrderLog = PurchaseOrderLogs(
+            po_id=instance,
+            additional_details=[{"key":"STATUS","value":validated_data.get("status")}],
+            comment="Purchase Order Created with Status: "+validated_data.get("status"),
+            created_by_user_id=validated_data.get("created_by_user_id"),
+            domain_user_id=validated_data.get("domain_user_id")
+        )
+        purchaseOrderLog.save()
+        return instance
+
+class PurchaseOrderListSerializer(serializers.ModelSerializer):
+    warehouse_id=serializers.CharField(source="warehouse_id.name", read_only=True)
+    supplier_id=serializers.CharField(source="supplier_id.email", read_only=True)
+    last_updated_by_user_id=serializers.CharField(source="last_updated_by_user_id.username", read_only=True)
+    created_by_user_id=serializers.CharField(source="created_by_user_id.username", read_only=True)
+    updated_by_user_id=serializers.CharField(source="updated_by_user_id.username", read_only=True)
+    domain_user_id=serializers.CharField(source="domain_user_id.username", read_only=True)
+    approved_by_user_id=serializers.CharField(source="approved_by_user_id.username", read_only=True)
+    cancelled_by_user_id=serializers.CharField(source="cancelled_by_user_id.username", read_only=True)
+    received_by_user_id=serializers.CharField(source="received_by_user_id.username", read_only=True)
+    returned_by_user_id=serializers.CharField(source="returned_by_user_id.username", read_only=True)
+    
+    class Meta:
+        model=PurchaseOrder
+        fields="__all__"
+    
+    
 
 class CreatePurchaseOrderView(generics.CreateAPIView):
     authentication_classes=[JWTAuthentication]
     permission_classes=[IsAuthenticated]
 
-    def get(self, request):
+    def get(self, request,id=None):
+        po = PurchaseOrder.objects.filter(
+            domain_user_id=request.user.domain_user_id.id,
+            id=id
+        ).first() if id else PurchaseOrder()
+        
+        poItems = PurchaseOrderItems.objects.filter(po_id=id) if id else []
+        poItems = PurchaseOrderItemSerializer(poItems, many=True).data
+        poData={"supplier_id":po.supplier_id.id, "supplier_email":po.supplier_id.email} if id else {}
         poFields = getDynamicFormFields(
-            PurchaseOrder(), 
+            po, 
             request.user.domain_user_id.id,
             skip_related=["supplier_id"],
             skip_fields=[
@@ -83,6 +155,8 @@ class CreatePurchaseOrderView(generics.CreateAPIView):
         )
         return renderResponse(
             data={
+                "poItems":poItems,
+                "poData":poData,
                 "poFields":poFields,
                 "poItemFields":poItemFields
             },
@@ -90,13 +164,24 @@ class CreatePurchaseOrderView(generics.CreateAPIView):
             status=200
         )
     
-    def post(self, request):
+    def post(self, request, id=None):
         data = request.data
         data.update({"created_by_user_id":request.user.id})
         data.update({"domain_user_id":request.user.domain_user_id.id})
         data.update({"last_updated_by_user_id":request.user.id})
-        
-        serializer = PurchaseOrderSerializer(data=data)
+
+        if id:
+            po = PurchaseOrder.objects.filter(
+                domain_user_id=request.user.domain_user_id.id,
+                id=id
+                ).first()
+            if po:
+                serializer=PurchaseOrderSerializer(po,data=data)
+            else:
+                return renderResponse(message="Purchase order Not Found", status=404)
+        else:
+            serializer = PurchaseOrderSerializer(data=data)
+
         if serializer.is_valid():
             serializer.save()
             return renderResponse(
@@ -109,3 +194,22 @@ class CreatePurchaseOrderView(generics.CreateAPIView):
             message="Error Creating Purchase Order",
             status=400
         )
+    
+
+
+class PurchaseOrderListView(generics.ListAPIView):
+    serializer_class = PurchaseOrderListSerializer  
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    pagination_class = CustomPageNumberPagination
+
+    def get_queryset(self):
+        queryset = PurchaseOrder.objects.filter(
+            domain_user_id=self.request.user.domain_user_id.id,
+        )
+        return queryset
+    
+     #using the mixin to add search and ordering functionality
+    @CommonListAPIMixin.common_list_decorator(PurchaseOrderListSerializer)
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
--- OrderServices/urls.py
+++ OrderServices/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from OrderServices.Controller.PurchaseOrderController import CreatePurchaseOrderView
+from OrderServices.Controller.PurchaseOrderController import CreatePurchaseOrderView, PurchaseOrderListView
 
 
 urlpatterns = [
     path("purchaseOrder/", CreatePurchaseOrderView.as_view(), name='purchase_order'),
+    path("purchaseOrder/<str:id>/", CreatePurchaseOrderView.as_view(), name='purchase_order_draft_details'),
+    path("purchaseOrderList/", PurchaseOrderListView.as_view(), name='purchase_order_list'),
+
 ]
